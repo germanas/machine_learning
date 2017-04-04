8603f296f4969c2310004fb642f4ae2fc7e48dc5
Germanas <germanas.latvaitis@gmail.com>
2017-04-04 19:19:17 +0300

    add poi

--- poi_id.py
+++ poi_id.py
@@ -6,60 +6,31 @@ sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
-import pprint
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-import seaborn as sns
 
-### Helper functions
-
-#plot features ####!!!! rebuild this function to take any 2 features and display a scatterplot
-def plotting_salary_expenses(dataset, feature1, feature2):
-    #print dataset
-    x = []
-    y = []
-    z = []
-    new_z = []
-    for person in dataset:
-        for feature in data_dict[person]:
-            if feature == feature1:
-                x.append(data_dict[person][feature])
-            elif feature == feature2:
-                y.append(data_dict[person][feature])
-            elif feature == 'poi':
-                z.append(data_dict[person][feature])
-    for i in z:
-        if i == True:
-            new_z.append('g')
-        elif i == False:
-            new_z.append('r')
-    fig, ax = plt.subplots()
-    for color in new_z:
-        ax.scatter(x, y, c=new_z)
-    #plt.scatter(x,y, c=z, label=z)
-    red_patch = mpatches.Patch(color='red', label='Poi TRUE')
-    green_patch = mpatches.Patch(color='green', label='Poi FALSE')
-    ax.legend(handles=[red_patch, green_patch])
-    ax.set_xlabel(feature1)
-    ax.set_ylabel(feature2)
-    plt.show()
+### Import helper functions
+from helper_functions import plotting_salary_expenses
+from helper_functions import dataset_info
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary','expenses'] # You will need to use more features
+features_list = ['poi','salary','expenses', 'from_poi_to_this_person', 'from_this_person_to_poi'] # You will need to use more features
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 #my_dataset = data_dict
-print data_dict["TOTAL"]
-### Task 2: Remove outliers
-plotting_salary_expenses(data_dict, 'salary', 'expenses')
-data_dict.pop("TOTAL", 0)
-plotting_salary_expenses(data_dict, 'salary', 'expenses')
 
+# Get info about this dataset
+dataset_info(data_dict)
+
+### Task 2: Remove outliers
+# I will plot different features to check for outliers
+plotting_salary_expenses(data_dict, 'salary', 'expenses')
+plotting_salary_expenses(data_dict, 'salary', 'from_poi_to_this_person')
+plotting_salary_expenses(data_dict, 'from_poi_to_this_person', 'from_this_person_to_poi')
+# From these plots I can see only one major outlier. So I remove it:
+data_dict.pop("TOTAL", 0)
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
@@ -67,7 +38,6 @@ my_dataset = data_dict
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
-
 
 
 ### Task 4: Try a varity of classifiers
@@ -79,7 +49,8 @@ labels, features = targetFeatureSplit(data)
 # Provided to give you a starting point. Try a variety of classifiers.
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
-
+clf.fit(labels, features)
+print clf.score(features)
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
